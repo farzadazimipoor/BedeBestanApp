d78b16c0925bb5bb413b5fb6d3cfddd02709833f
Farzad Azimipoor <farzad.azimipoor@live.com>
2015-11-04 19:43:53 +0330

    Activities and layouts Created SuccessFully

---
+++ app/src/main/java/com/sazenama/bedebestanapp/ShowPersonsList.java
@@ -0,0 +1,13 @@
+package com.sazenama.bedebestanapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowPersonsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.showlist);
+    }
+}
